4e2f44effb6393019de638f16a1abb4401ed3e2d
alanyuwenche <alanyuwenche@gmail.com>
2022-11-21 09:07:09 +0800

    Add files via upload

--- algos/erl_trainer.py
+++ algos/erl_trainer.py
@@ -166,7 +166,7 @@ class ERL_Trainer:
 			test_mean = np.mean(test_scores); test_std = (np.std(test_scores))
 			tracker.update([test_mean], self.total_frames)
 
-			if (test_N > 4) and (no_T > 1):
+			if (test_N > 1) and (no_T > 2):
 			#if test_N > 6:
 				f = open("./data/logfile.txt","a")
 				f.write('Gen: %d\t' % gen)
